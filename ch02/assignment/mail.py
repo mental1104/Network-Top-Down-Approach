7c5515d77454dda6cc621f9e2ade44693fa95920
mental1104 <mental1104@gmail.com>
2021-08-23 21:16:40 +0800

    broken smtp

---
+++ ch02/assignment/mail.py
@@ -0,0 +1,40 @@
+#作业3：邮件客户
+from smtplib import SMTP
+from email.mime.text import MIMEText
+from email.header import Header
+
+mail_server = 'smtp.qq.com'
+#根据发送方邮箱确定邮箱服务器
+#qq邮箱的服务器为smtp.qq.com;163邮箱为smtp.163.com
+def get_mail_server(sender):
+	key = sender[sender.index('@')+1:]
+	return "smtp."+key
+
+port = '25'  ## SMTP协议默认端口是25
+sender = '********************8'
+mail_server = get_mail_server(sender)
+sender_pass = '*****************'    #注意是授权码,而不是登录密码,需要在邮箱端先获取
+receiver = '****************'
+mail_msg = 'this is a demo'
+
+#第一个参数就是邮件正文，
+# 第二个参数是MIME的subtype，传入'plain'表示纯文本，最终的MIME就是'text/plain'，
+# 最后一定要用utf-8编码保证多语言兼容性。
+msg = MIMEText(mail_msg, 'plain', 'utf-8')
+msg['From'] = sender
+msg['To'] = receiver
+#Header对象编码文本，包含utf-8编码信息和Base64编码。
+msg['Subject'] = Header('Deep Dark Fantasy', 'utf-8')
+for i in range(1,21):
+    try:
+	    server = SMTP(mail_server, port)
+	    #用set_debuglevel(1),可以打印出和SMTP服务器交互的所有信息
+	    #server.set_debuglevel(1)
+	    server.login(sender, sender_pass)
+	    #由于可以一次发给多个人，所以传入一个list，邮件正文是一个str，as_string()把MIMEText对象变成str
+	    server.sendmail(sender, (receiver), msg.as_string() )
+	    server.quit()
+	    print("邮件发送成功！")
+    except:
+	    server.quit()
+	    print("邮件发送失败！")
--- ch02/readme.md
+++ ch02/readme.md
@@ -80,4 +80,19 @@ the server code.
 
 - Corresponding server: [Server.py](./assignment/Server.py)  
 
-![](./assignment/pic/ping.png)
+![](./assignment/pic/ping.png)  
+
+### Assignment 3: Mail Client  
+
+The goal of this programming assignment is to create a simple mail client that sends
+e-mail to any recipient. Your client will need to establish a TCP connection with
+a mail server (e.g., a Google mail server), dialogue with the mail server using the
+SMTP protocol, send an e-mail message to a recipient (e.g., your friend) via the mail
+server, and finally close the TCP connection with the mail server.
+For this assignment, the Companion Website provides the skeleton code for
+your client. Your job is to complete the code and test your client by sending e-mail
+to different user accounts. You may also try sending through different servers (for
+example, through a Google mail server and through your university mail server).  
+
+[mail.py](./assignment/mail.py)  
+
